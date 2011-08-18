0504097aa52783a7af8f8ead96debaeed453ccad
Stefan Armbruster <stefan@armbruster-it.de>
2011-08-18 09:38:30 +0200

    modify graphDatabaseSerivce bean to use HABugWorkaroundGraphDatabaseWrapper in grails.neo4j.type=='ha-workaround' in config

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,13 @@
-// Place your Spring DSL code here
+import org.codehaus.groovy.grails.commons.ApplicationHolder
+
 beans = {
+    def neo4jConfig = ApplicationHolder.application.config?.grails?.neo4j
+
+    if (neo4jConfig.type == 'ha-workaround') {
+        // override the bean definition from neo4j plugin
+        graphDatabaseService(org.neo4j.wrap.workaround.HABugWorkaroundGraphDatabaseWrapper, neo4jConfig.location, neo4jConfig.params) { bean ->
+            bean.destroyMethod = "shutdown"
+        }
+    }
+
 }
