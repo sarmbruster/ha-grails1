4e4f18035321020181e4af2ab9e5bff669567c53
Stefan Armbruster <stefan@armbruster-it.de>
2011-08-18 10:22:34 +0200

    if grails.neo4j.type maps to a classname, use it as graphDatabaseService implementation, adding sample for externalized config

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,13 +1,19 @@
 import org.codehaus.groovy.grails.commons.ApplicationHolder
+import org.codehaus.groovy.runtime.typehandling.GroovyCastException
+import org.neo4j.graphdb.GraphDatabaseService
 
 beans = {
     def neo4jConfig = ApplicationHolder.application.config?.grails?.neo4j
 
-    if (neo4jConfig.type == 'ha-workaround') {
-        // override the bean definition from neo4j plugin
-        graphDatabaseService(org.neo4j.wrap.workaround.HABugWorkaroundGraphDatabaseWrapper, neo4jConfig.location, neo4jConfig.params) { bean ->
+    try {
+        Class graphDatabaseServiceClass = neo4jConfig.type as Class
+        assert GraphDatabaseService.isAssignableFrom(graphDatabaseServiceClass)
+        // override the bean definition from neo4j plugin if 'type' is a valid clazz
+        graphDatabaseService(graphDatabaseServiceClass, neo4jConfig.location, neo4jConfig.params) { bean ->
             bean.destroyMethod = "shutdown"
         }
+    } catch (GroovyCastException e) {
+        log.info "using ${ref('graphDatabaseService')} as configured from plugin"
     }
 
 }
---
+++ ha-grails1-config.groovy
@@ -0,0 +1,27 @@
+// this is an example for an externalized config of a grails application
+grails {
+    neo4j {
+
+        type = "embedded"
+        location = "data/neo4j"
+        params = [:]
+
+//        type = "ha"
+//        location = "data/neo4j_" + System.properties['ha.machine_id']
+//        params = [
+//                'ha.machine_id': System.properties['ha.machine_id'],
+//                'ha.zoo_keeper_servers': 'localhost:2181,localhost:2182,localhost:2183'
+//        ]
+
+    }
+}
+/*grails {
+    neo4j {
+        type = 'org.neo4j.wrap.workaround.HABugWorkaroundGraphDatabaseWrapper'
+        location = "/home/stefan/Programming/mindblast/neoha/ha-grails1/data/neo4j"
+        params = [
+                'ha.machine_id': '1',
+                'ha.zoo_keeper_servers': 'localhost:2181,localhost:2182,localhost:2183'
+        ]
+    }
+}*/
